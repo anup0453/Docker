5db7820d3eeb47c07de8ddc8750b08d91727e92b
anup0453 <101499158+anup0453@users.noreply.github.com>
2022-11-24 22:27:26 +0530

    Docker installaion file

--- Docker_install.sh
+++ Docker_install.sh
@@ -1,12 +1,16 @@
-//Uninstall old versions
-sudo apt-get remove docker docker-engine docker.io containerd runc
-
-//Set up the repository
-//Update the apt package index and install packages to allow apt to use a repository over HTTPS:
 sudo apt-get update
+
 sudo apt-get install \ca-certificates \curl \gnupg \lsb-release
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
    
    
-  //To install the latest version, run:
-  sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
-  docker --version
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+docker --version
